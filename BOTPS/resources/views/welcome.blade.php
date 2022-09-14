6ba860a2011737cd1bb50cacf60682c60efbcb7c
Kittiphum <76035271+kittiphum717@users.noreply.github.com>
2022-09-14 20:26:26 +0700

    update docker

--- BOTPS/resources/views/welcome.blade.php
+++ BOTPS/resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>BOT-PS</title>
 
         <!-- Fonts -->
         <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
@@ -33,14 +33,16 @@
                     </svg>
                 </div>
 
+
+
+                <!-- text block (Click Me)-->
                 <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
                     <div class="grid grid-cols-1 md:grid-cols-2">
                         <div class="p-6">
                             <div class="flex items-center">
                                 <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8 text-gray-500"><path d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253"></path></svg>
-                                <div class="ml-4 text-lg leading-7 font-semibold"><a href="https://youtu.be/UIp6_0kct_U" class="underline text-gray-900 dark:text-white">Click Me</a></div>
+                                <div class="ml-4 text-lg leading-7 font-semibold"><a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" class="underline text-gray-900 dark:text-white">Click Me</a></div>
                             </div>
-
                             <div class="ml-12">
                                 <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
                                     Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quo corporis laborum ut commodi? Voluptatibus perferendis, quidem inventore autem dignissimos, aliquid officiis fugiat repellendus illo consectetur molestias rem ipsum qui possimus.
