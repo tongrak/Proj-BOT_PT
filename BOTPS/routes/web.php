d25c385af39bf7ec4691e6d568d1d4e4e34c876c
Bank <deknan70@gmail.com>
2022-09-20 22:52:02 +0700

    add login and register route

--- BOTPS/routes/web.php
+++ BOTPS/routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+/**login route */
+Route::get('/login',function() { return view('login');});
+
+/**register route */
+Route::get('register',function() { return view('register');});
