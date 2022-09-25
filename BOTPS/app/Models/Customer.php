7acc54db7d8d450009145a02eca7a720756f0e4c
Oliver Tongrak <tongrak_ru@cmu.ac.th>
2022-09-25 21:07:22 +0700

    Model Foothold Commit

---
+++ BOTPS/app/Models/Customer.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Customer extends Model
+{
+    // use HasFactory;
+    protected $fillable = [
+        'customerNumber',
+        'customerName',
+        'customerLastName',
+        'customerFirstName',
+        'phone',
+        'addressLine1',
+        'addressLine2',
+        'city',
+        'state',
+        'postalCode',
+        'country',
+        'salesRepEmployeeNumber',
+        'creditLimit'
+    ];
+}
