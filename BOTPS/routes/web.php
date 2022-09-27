79d365e694a0e85428f9633bfc823074aabcfdb5
Oliver Tongrak <tongrak_ru@cmu.ac.th>
2022-09-27 15:33:49 +0700

    adding controller based routing

--- BOTPS/routes/web.php
+++ BOTPS/routes/web.php
@@ -13,9 +13,46 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['namespace' => 'App/Http/Controllers'], function(){
+    // Homepage route
+    Route::get('/', function () {return view('home');});
+    // catalog route
+    Route::get('/catalog', 'CatalogController@show')->name('catalog.show');
+    // client as guest
+    Route::group(['middleware'=>['guest']], function(){
+        // Registing route
+        Route::get('/register', 'RegisterController@show')->name('register.show');
+        Route::post('/register', 'RegisterController@register')->name('register.perform');
+        // Login route
+        Route::get('/login', 'LoginController@show')->name('login.show');
+        Route::post('/login', 'LoginController@login')->name('login.perform');
+    }); 
+    // client as customer
+    Route::group(['middleware' => ['customer']], function(){
+        // Auth routing
+        Route::get('/logout', 'LogoutController@perform')->name('logout.perform');
+        // shop routing:
+        // cart routing:
+        Route::get('/cart', 'ProductController@cart')->name('cart');
+        Route::get('/add-t-cart/{id}', 'ProductController@addToCart')->name('add.to.cart');
+        Route::delete('/remove-from-cart/{id}', 'ProductController@remove')->name('remove.from.cart');
+        // contact employee route:
+        Route::post('/confirm-cart/{id}', 'CartController@confirm')->name('confirm.cart');
+        Route::post('/confirm-payment/{id}', 'CartController@endComm')->name('confirm.payment');
+    });
+    // client as employee
+    Route::group(['middleware'=>['employee']], function(){
+        // commsion broads route:
+        Route::get('/commissions', 'CommissionController@show')->name('commission.show');
+        // commsion action route:
+        Route::post('/confirm-commis/{id}', 'CommissionController@confirm')->name('commission.confirm');
+        Route::post('/confirm-comm-end/{id}', 'CommissionController@endComm')->name('commission.end.comm');
+
+    });
+
+
 });
+
 
 /**login route */
 Route::get('/login',function() { return view('login');});
