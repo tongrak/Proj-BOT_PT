d75f82f6a708dc41cd16cdd386e0ba5712b224b5
tanhatai159 <gun159@outlook.com>
2022-09-29 20:06:25 +0700

    fix path of route group

--- BOTPS/routes/web.php
+++ BOTPS/routes/web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::group(['namespace' => 'App/Http/Controllers'], function(){
+Route::group(['namespace' => 'App\Http\Controllers'], function(){
     // Homepage route
     Route::get('/', function () {return view('home');});
     // catalog route
