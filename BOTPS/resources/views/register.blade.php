cfeb33b6cac4befc3b80805eeef646d986b0503e
Bank <deknan70@gmail.com>
2022-10-09 21:01:35 +0700

    register page from figma

--- BOTPS/resources/views/register.blade.php
+++ BOTPS/resources/views/register.blade.php
@@ -2,44 +2,114 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta charset="utf-8" />
+    <title>register_page</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Register</title>
-    @vite('resources/css/app.css')
+    <link rel="StyleSheet" href="{{ asset('css/register.css') }}" />
+    <script src="https://secure.exportkit.com/cdn/js/ek_googlefonts.js?v=6"></script>
+    <!-- Add your custom HEAD content here -->
+
 </head>
 
 <body>
-    <!-- navBar -->
-    <div class="justify-start flex bg-blue-300 pl-5 py-2 space-x-4">
-        <a href="{{ url('/home') }}">
-            <h1 class="text-xl font-bold font-mono text-gray-100">Home</h1>
-        </a>
-        <a href="{{ url('/login') }}">
-            <h1 class="text-xl font-bold font-mono text-gray-100">Login</h1>
-        </a>
-        <a href="{{ url('/register') }}">
-            <h1 class="text-xl font-bold font-mono text-gray-100">Register</h1>
-        </a>
-    </div>
+    <div id="content-container">
+        <div id="_bg__register_page"></div>
 
-    <!-- box-text-1 -->
-    <div class="mt-8 rounded-lg bg-gray-100 mx-80">
-        <div class="m-5 py-2 grid-flow-row auto-rows-max">
+        <div id="master_of_all_page">
+            <div id="rectangle_1"></div>
+            <div id="rectangle_2"></div>
+            <div id="team_ps_bot">
+                Team PS-BOT
+            </div>
+            <div id="rectangle_3"></div>
+            <div id="ps_bot_shop">
+                PS BOT<br />SHOP
+            </div>
 
-            <div class="">
-                <h1 class="text-xl">Register</h1>
-            </div>
-            <div class="">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut, fugit. Quod ea repellendus, possimus fugiat natus quae ullam veniam vitae rem sapiente illo aspernatur eum nesciunt dolorum deleniti facilis quisquam quis saepe voluptates provident? Culpa provident necessitatibus asperiores itaque ab nobis eaque dolore ipsum, deleniti nemo quasi dolorum debitis hic.
-            </div>
+            <a id="frame_1_ek3_cls_a" _par="" href="home">
+                <div id="Home_btn"></div>
+                <div id="cart_ek3">
+                    Home
+                </div>
+
+            </a>
+            <img src="skins/logo_1.png" id="logo_1" />
+            <img src="skins/logo_2.png" id="logo_2" />
 
         </div>
+        <img src="skins/line_2.png" id="line_2" />
+        <div id="already_have_an_account_">
+            Already have an account?
+        </div>
+
+        <a id="frame_2_cls_a" _par="" href="login">
+            <div id="login_here"></div>
+            <div id="cart_ek4">
+                Login here
+            </div>
+        </a>
+
+        <div id="sign_up">
+            Sign-up
+        </div>
+
+
+        <div id="username">
+            Username
+        </div>
+        <div id="password">
+            Password
+        </div>
+        <div id="first_name">
+            First Name
+        </div>
+
+        <div id="last_name">
+            Last Name
+        </div>
+        <div id="phone">
+            Phone
+        </div>
+        <div id="rectangle_6"></div>
+        <div id="rectangle_15"></div>
+        <img src="skins/rectangle_7.png" id="rectangle_7" />
+        <img src="skins/rectangle_16.png" id="rectangle_16" />
+        <div id="rectangle_8"></div>
+
+
+        <div id="rectangle_9"></div>
+        <img src="skins/rectangle_10.png" id="rectangle_10" />
+        <div id="rectangle_11"></div>
+        <img src="skins/rectangle_12.png" id="rectangle_12" />
+        <div id="rectangle_13"></div>
+        <div id="rectangle_14"></div>
+
+        <a id="frame_1_ek4_cls_a" _par="" href="register">
+            <div id="cart_ek5">
+                Sign-up
+            </div>
+
+        </a>
+
+
+
+        <div id="address">
+            Address
+        </div>
+        <div id="city">
+            City
+        </div>
+        <div id="state">
+            State
+        </div>
+        <div id="postalcode">
+            PostalCode
+        </div>
+        <div id="country">
+            Country
+        </div>
+
     </div>
-
-    
-
-
 </body>
 
 </html>
