01815d948f839826922fc488d12496f2cad93998
iolq <wipawee_watt@cmu.ac.th>
2022-10-16 21:22:19 +0700

    catalog page updated detail added

--- BOTPS/resources/views/Catalog.blade.php
+++ BOTPS/resources/views/Catalog.blade.php
@@ -15,18 +15,59 @@
 </head>
 
 <body>
-    <div class="flex flex-auto justify-center bg-gray-700">
-        <!-- <a href="{{ config('app.url')}}/products">Back</a> -->
-        <h1>Here lay backend-dev's hope and dream for </h1>
+    <!-- navBar -->
+    <div class="flex bg-blue-300 pl-5 py-2 space-x-4">
+        <a href="{{ url('/home') }}">
+            <h1 class="text-xl font-bold font-mono text-gray-100 justify-start">Home</h1>
+        </a>
+        <a href="{{ url('/login') }}">
+            <h1 class="text-xl font-bold font-mono text-gray-100 justify-end">Login</h1>
+        </a>
+        <a href="{{ url('/register') }}">
+            <h1 class="text-xl font-bold font-mono text-gray-100">Register</h1>
+        </a>
     </div>
-    <div class="grid grid-cols-1 justify-center">
-        @foreach($products as $product)
-        <div class="bg-gray-700">
-            <h4>Name: {{$product->productName}}</h4>
-            <p>Code: {{$product -> productCode}}</p>
+
+    <div class="m-5 py-2 grid-flow-row auto-rows-max">
+        <h1 class="text-xl">Product in Catalog</h1>
+    </div>
+
+    <div class="mt-8 rounded-lg bg-gray-100 mx-80">
+        <div class="m-5 py-2">
+            <row>
+                <col>
+                Classic cars |
+                </col>
+                <col>
+                Motorcycles |
+                </col>
+                <col>
+                Plains |
+                </col>
+                <col>
+                Ships
+                </col>
+            </row>
         </div>
-        @endforeach
+        <div class="m-5 py-2 grid-flow-row auto-rows-max">
+            <row>
+                <col>
+                Trains |
+                </col>
+                <col>
+                Trucks & Buses |
+                </col>
+                <col>
+                Vintage cars |
+                </col>
+                <col>
+                More
+                </col>
+            </row>
+        </div>
     </div>
+    </div>
+
 
 </body>
 
