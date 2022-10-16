aea17af869a2d8915701701bc4ab79ed9fbed730
Oliver Tongrak <tongrak_ru@cmu.ac.th>
2022-10-16 22:27:09 +0700

    CartController: Foothold implementation

--- BOTPS/app/Http/Controllers/ProductController.php
+++ BOTPS/app/Http/Controllers/ProductController.php
@@ -6,6 +6,7 @@ namespace App\Http\Controllers;
 use App\Models\Product;
 use App\Models\Cart;
 use Illuminate\Contracts\Session\Session;
+// use App\Http\Controllers\Session;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 
@@ -14,14 +15,6 @@ class ProductController extends Controller
     public function showCatalog(){
         $products = Product::all();
         return view('Catalog', compact('products'));
-    }
-
-    public function cart($cusId){
-        $carts = DB::select('
-            select *
-            from carts
-            where customerNumber like ' + $cusId);
-        return view('cart', ['carts'=>$carts]);
     }
 
     public function showHome(){
@@ -43,36 +36,6 @@ class ProductController extends Controller
         return view('Catalog', compact('products'));
     }
 
-    // !! Moving to Cart Controller.
-    // public function addToCart($productId){
-    //     $product = Product::findOrFail($productId);
-    //     // $cart = Cart::where('customerNumber', '=', $cusId)->first();
-    //     if (!Session::has('login-id')) return view('login');
-    //     $cusId = Session::get('login-id');
-    //     $carts = DB::select('
-    //         select *
-    //         from carts
-    //         where customerNumber like ' + $cusId +
-    //         'and productNumber like ' + $productId
-    //     );
-    //     $cart = $carts[0];
-    //     DB::transaction(function()use($product, $cart , $cusId){
-    //         if($cart != null){
-    //             $cart->quantity         = $cart->quantity+1;
-    //             $cart->save();
-    //         }else{
-    //             $cart = new Cart();
-    //             $cart->customerNumber   = $cusId;
-    //             $cart->productNumber    = $product->productCode;
-    //             $cart->quantity         = 1;
-    //             $cart->pricePerUnit     = $product->buyPrice;
-    //             $cart->confirmation     = false;
-    //             $cart->save();
-    //         }
-    //         $product->quantityInStock = $product->quantityInStock-1;
-    //         $product->save();
-    //     });
-    //     return redirect()->back()->with('success', 'product added successfully ');
-    // }
+    
 
 }
