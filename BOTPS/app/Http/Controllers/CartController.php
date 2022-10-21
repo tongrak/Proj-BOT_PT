374540ed56f422397bf4369371abb6a25dccc8f9
Oliver Tongrak <tongrak_ru@cmu.ac.th>
2022-10-21 18:21:31 +0700

    CartController: Untested showCartOfSaleRep impl

--- BOTPS/app/Http/Controllers/CartController.php
+++ BOTPS/app/Http/Controllers/CartController.php
@@ -90,21 +90,35 @@ class CartController extends Controller
         $this->showCartDetail($cusId);
     }
 
-    private function getCartFullDetail($salerepID = null){
+    private function getCartOfSaleRep($salerepID = null){
         $val = $salerepID;
         if(isNull($salerepID)) $val = 'NULL';
-        $toRe = DB::select('
-            SELECT customerNumber, productCode, quantity
-            FROM (SELECT * FROM carts WHERE salerepNumber like '.$val.') as C
-            JOIN cartdetails ON C.cartNumber = cartdetails.cartNumber
-        ');
+
+        $customers = DB::select('
+            SELECT cartNumber, customerNumber
+            FROM carts
+            WHERE salerepNumber = '.$val
+        );
+        $toRe = array("Temp");
+        foreach ($customers as $customer => $cartNumber) {
+            $res = DB::select('
+                SELECT productCode, quantity
+                FROM cartdetails
+                WHERE cartNumber = '.$cartNumber
+            );
+            if ($toRe[0] == "Temp") {
+                $toRe = array($cartNumber=>$res);
+            }else{
+                array_push($toRe,array($cartNumber=>$res));
+            }
+        }
         return $toRe;
     }
 
-    public function showCart($salerepID){
-        $cartNoRep = $this->getCartFullDetail();
-        $cartWithRep = $this->getCartFullDetail($salerepID);
-        return view('home',compact($cartNoRep),compact($cartWithRep));
+    public function showCartOfSaleRep($salerepID){
+        $cartNoRep = $this->getCartOfSaleRep(null);
+        $cartWithRep = $this->getCartOfSaleRep($salerepID);
+        // return view('home',compact($cartNoRep),compact($cartWithRep));
     }
 
     
