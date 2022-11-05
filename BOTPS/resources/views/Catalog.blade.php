d29f6d2bd09473c1c2de98535f248cfe126b3f66
Kittiphum <76035271+kittiphum717@users.noreply.github.com>
2022-11-05 23:13:40 +0700

    update Catalog

--- BOTPS/resources/views/Catalog.blade.php
+++ BOTPS/resources/views/Catalog.blade.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="UTF-8">
-    <title>home_page</title>
+    <title>Catalog</title>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/css/slick.css">
@@ -17,12 +17,13 @@
 
 
 <body>
+    <!-- navBar -->
     <div class="nav">
 
         <!-- 2 box -->
-        <div class="flex space-x-16 ">
+        <div class="flex space-between">
             <a href="{{ url('/') }}" class="px-5">
-                <div class="text-fuchsia-600 bg-white px-9 py-6">
+                <div class="text-fuchsia-600 bg-white px-9 h-20 pt-2 ">
                     <h1 class="text-lg font-medium">PS BOT</h1>
                     <h1 class="pl-2 text-lg font-medium">SHOP</h1>
                 </div>
@@ -50,6 +51,7 @@
                 <h1 class="btn_a">Logout</h1>
             </a>
         </div>
+
 
         @else
         <div class="flex space-x-4 pt-5 pr-5">
@@ -81,7 +83,7 @@
             <h1 class="new_customer">Products in Catalog</h1>
         </div>
 
-        <div class="comm_div">
+        <!-- <div class="comm_div">
             <div class="m-5 py-2">
                 <row>
                     <col>
@@ -112,9 +114,54 @@
 
                 </row>
             </div>
+        </div> -->
+
+
+        @foreach($products as $product)
+        <div class="comm_div">
+            <div class="m-5 py-2">
+                <row>
+                    <col>
+                    <button>Name: {{$product->productName}}</button>
+                    </col>
+                    <col>
+                    <button>Code: {{$product -> productCode}}</button>
+                    </col>
+                    <col>
+                    <button>Line: {{$product -> productLine}}</button>
+                    </col>
+                    <col>
+                    <button>Scale: {{$product -> productScale}}</button>
+                    </col>
+                </row>
+            </div>
+            <div class="m-5 py-2 grid-flow-row auto-rows-max">
+                <row>
+                    <col>
+                    <button>Vendor : {{$product -> productVendor}}</button>
+                    </col>
+                    <col>
+                    <button>Description: {{$product -> productDescription}}</button>
+                    </col>
+                    <col>
+                    <button>Stock: {{$product -> quantityInStock}}</button>
+                    </col>
+                    <col>
+                    <button>Price: {{$product -> buyPrice}}</button>
+                    </col>
+                    <div class="confirm_btn">
+                        <p><a href="{{route('add.to.cart', $product->productCode)}}">Add to cart</a></p>
+
+                    </div>
+                </row>
+            </div>
         </div>
-        <div class="m-5 py-2 grid-flow-row auto-rows-max">
-            @foreach($products as $product)
+
+        @endforeach
+
+        <!-- @foreach($products as $product)
+        <div class="mx-5 py-2 grid-flow-row auto-rows-max ">
+           
             <div class="bg-gray-700">
                 <h4>Name: {{$product->productName}}</h4>
                 <p>Code: {{$product -> productCode}}</p>
@@ -123,9 +170,10 @@
 
                 </div>
             </div>
-
-            @endforeach
         </div>
+        @endforeach -->
+
+
         <!-- copyrigth -->
         <div class="bg-slate-500 flex justify-center place-self-end h-10">
             <img src="skins/logo_2.png" id="logo_2" />
