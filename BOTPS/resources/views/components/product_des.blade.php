bc5c5bf4bdfaeee803d7cd7df2edf2401f890993
Bank <deknan70@gmail.com>
2022-11-06 16:26:08 +0700

    commision page adjust text

--- BOTPS/resources/views/components/product_des.blade.php
+++ BOTPS/resources/views/components/product_des.blade.php
@@ -1,7 +1,7 @@
 <div style="display: flex;">
-    <div class="product_description">-Product Code : {{$c->productCode}}</div>
-    <div class="product_description">-Product Name : {{$c->productName}}</div>
+    <div class="product_code">-Product Code : {{$c->productCode}}</div>
+    <div class="product_name">-Product Name : {{$c->productName}}</div>
     <div class="quantity">Quantity: {{$c->quantity}}</div>
-    <div class="price">Price: {{$c->pricePerUnit}}</div>
-    <div class="price">Total Price: {{$c->totalPrice}}</div>
+    <div class="price">Price: {{$c->pricePerUnit}} $</div>
+    <div class="price">Total Price: {{$c->totalPrice}} $</div>
 </div>
