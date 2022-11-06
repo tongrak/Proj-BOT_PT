f21679b4ec028bb6128d0913b03f1ff9ef9cca5f
Bank <deknan70@gmail.com>
2022-11-07 00:38:27 +0700

    completed box

--- BOTPS/resources/views/components/botton.blade.php
+++ BOTPS/resources/views/components/botton.blade.php
@@ -1,22 +1,36 @@
 <div class="btn_frame">
-    <form method="post" action="{{ route('admin.cancel.order', key($cusID)) }}">
+    <form method="post" action="{{ route('admin.cancel.order', key($cusID)) }} " onclick="cancel()">
         @csrf
         <button class="cancel_btn">
             Cancel Order
         </button>
     </form>
 
-    <form method="post" action="{{ route('admin.denied.order', key($cusID)) }}">
+    <form method="post" action="{{ route('admin.denied.order', key($cusID)) }}" onclick="deniend()">
         @csrf
         <button class="denied_btn">
             Denied Order
         </button>
     </form>
 
-    <form method="post" action="{{ route('commission.confirm', key($cusID)) }}">
+    <form method="post" action="{{ route('commission.confirm', key($cusID)) }}" onclick="confirm()">
         @csrf
         <button class="confirm_btn">
             Confirm Order
         </button>
     </form>
+
+    <script>
+        function cancel() {
+            alert("Cancel completed");
+        }
+
+        function confirm() {
+            alert("Confirm completed");
+        }
+
+        function deniend() {
+            alert("Deniend completed");
+        }
+    </script>
 </div>
