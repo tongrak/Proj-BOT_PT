f869c84549d04c06f2b36c11208b80e888a2413e
Bank <deknan70@gmail.com>
2022-11-06 21:53:39 +0700

    cancel order button for admin

--- BOTPS/resources/views/components/botton.blade.php
+++ BOTPS/resources/views/components/botton.blade.php
@@ -1,8 +1,22 @@
 <div class="btn_frame">
-    <a href="home" class="cancel_btn">
-        Cancel Order
-    </a>
-    <a href="home" class="confirm_btn">
-        Confirm Order
-    </a>
+    <form method="post" action="{{ route('admin.cancel.order', key($cusID)) }}">
+        @csrf
+        <button class="cancel_btn">
+            Cancel Order
+        </button>
+    </form>
+
+    <form action="">
+        @csrf
+        <button class="denied_btn">
+            Denied Order
+        </button>
+    </form>
+
+    <form action="">
+        @csrf
+        <button class="confirm_btn">
+            Confirm Order
+        </button>
+    </form>
 </div>
