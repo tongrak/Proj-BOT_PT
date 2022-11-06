ae3470dbe347749e72f155a6343b7268c3c0c54d
Kittiphum <76035271+kittiphum717@users.noreply.github.com>
2022-11-06 17:18:32 +0700

    cart update

--- BOTPS/resources/views/cart.blade.php
+++ BOTPS/resources/views/cart.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <title>Cart_page</title>
+    <link rel="icon" href="/skins/logo_3.png" type="image/x-icon">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/css/slider.css">
@@ -21,35 +22,48 @@
         <div class="nav">
 
             <!-- 2 box -->
-            <div class="flex space-x-16 ">
-                <div class="text-fuchsia-600 bg-white px-5 pt-3 mb-1">
-                    <h1 class="">PS BOT</h1>
-                    <h1 class="pl-2">SHOP</h1>
-                </div>
+            <div class="flex space-between">
+                <a href="{{ url('/') }}" class="px-5">
+                    <div class="text-fuchsia-600 bg-white px-9 h-20 pt-2 ">
+                        <h1 class="text-lg font-medium">PS BOT</h1>
+                        <h1 class="pl-2 text-lg font-medium">SHOP</h1>
+                    </div>
+                </a>
+
                 <div class="pt-5">
-                    <a href="{{ url('/home') }}">
-                        <h1 class="btn_a">Home</h1>
-                    </a>
+                    @if(Session::has('login-id'))
+                    <p class="text-white text-2xl ring-1 rounded-2xl px-5 ring-white mt-2"> ID : {{Session::get("login-id")}}</p>
+                    @endif
                 </div>
             </div>
 
             <!-- logo -->
-            <div class="pr-10">
-                <img src="skins/logo_1.png" id="logo_1" />
+            <div class="logo">
+                <img src="/skins/logo_1.png" id="logo_1" />
             </div>
 
-            <!-- 3 box -->
+            <!-- check role -->
+            @if(Session::has('login-id'))
             <div class="flex space-x-4 pt-5 pr-5">
-                <!-- <a href="{{ url('/login') }}">
-                    <h1 class="text-xl font-bold font-sans text-white rounded-2xl px-6 py-2.5 ring-1 ring-white transition ease-in-out  hover:-translate-y-1 hover:scale-110 duration-100">Login</h1>
+                <a href="{{ url('/cart') }}">
+                    <h1 class="btn_a">Cart</h1>
                 </a>
-                <a href="{{ url('/register') }}">
-                    <h1 class="text-xl font-bold font-sans text-white rounded-2xl px-6 py-2.5 ring-1 ring-white transition ease-in-out  hover:-translate-y-1 hover:scale-110 duration-100">Sign-up</h1>
-                </a> -->
-                <a href="{{ url('/home') }}">
-                    <h1 class="btn_a">logout</h1>
+                <a href="{{ url('/logout') }}">
+                    <h1 class="btn_a">Logout</h1>
                 </a>
             </div>
+
+            @else
+            <div class="flex space-x-4 pt-5 pr-5">
+                <a href="{{ url('/login') }}">
+                    <h1 class="btn_a">Login</h1>
+                </a>
+                <a href="{{ url('/register') }}">
+                    <h1 class="btn_a">Sign-up</h1>
+                </a>
+            </div>
+            @endif
+
         </div>
 
         <!-- contents -->
@@ -63,31 +77,50 @@
             <!-- contents -->
             <div class="">
 
-                <!-- box 1
-                <div class="bg-slate-200 flex justify-center mx-56 mt-10">
-                    <p class="p-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere corrupti praesentium adipisci, est eveniet rem accusantium assumenda sed. Ad eveniet numquam, hic et possimus ea nobis neque saepe consequatur praesentium!</p>
-                </div> -->
+                <!-- @for($i = 0;$i < 10;$i++)
+                <div class="cart">
+                    <div class="space-y-1 py-3">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, sed?</p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, sed?</p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, sed?</p>
+                    </div>
+                    <button class="del">Delete</button>
+                </div>
+                @endfor -->
 
-                @foreach( $cartdetails as $cart )
-                <tr>
-                    @php $total += $cart->price*$cart->quantity @endphp
-                    <td>{{ $cart->name }}</td>
-                    <td>{{ $cart->price }}</td>
-                    <td>{{ $cart->quantity }}
-                    <td data-th="Subtotal" class="">{{$cart->price*$cart->quantity}}</td>
-                </tr>
+                @if($cartDetails != null)
+                @foreach($cartDetails as $cart)
+                <div class="cart">
+
+                    <!-- text -->
+                    <div class="space-y-1 py-3 pt-8 text-xl">
+                        <!-- <p>customerNumber: {{$cart->customerNumber}}</p> -->
+                        <p>productCode: {{$cart->productCode}}</p>
+                        <p>quantity: {{$cart->quantity}}</p>
+                    </div>
+                    <form method="POST" action="{{route('remove.from.cart', $cart->productCode)}}">
+                        @csrf
+                        @method('DELETE')
+                        <button class="del">Delete</button>
+                    </form>
+
+                </div>
                 @endforeach
-
+                @endif
 
 
             </div>
 
             <!-- order button -->
-            <div class="flex">
-                <div class="bottom-0 right-10 absolute">
-                    <button class="text-white rounded-md py-2 px-5 bg-green-400 hover:bg-green-600 active:bg-green-500 text-xl">Order</button>
+            <form method="post" action="{{route('confirm.order')}}">
+            @csrf
+                <div class="flex">
+                    <div class="bottom-0 right-10 absolute">
+                        <button class="text-white rounded-md py-2 px-5 bg-green-400 hover:bg-green-600 active:bg-green-500 text-2xl">Order</button>
+                    </div>
                 </div>
-            </div>
+            </form>
+
 
         </div>
         <!-- copyrigth -->
